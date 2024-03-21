9b3b4747bc2a89634f05916181de6d3378268936
Elsa <sasa.milhet@gmail.com>
2024-03-21 18:48:38 +0100

    menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,45 +1,84 @@
 import React from 'react';
+import { AppBar, Typography, Toolbar, IconButton, MenuItem, Menu, InputBase } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
-import Drawer, { DrawerContent, DrawerHeader, DrawerTitle, DrawerSubtitle} from '@material/react-drawer';
-import List, {ListItem,ListItemGraphic,ListItemText} from '@material/react-list';
-import MaterialIcon from '@material/react-material-icon';
-import {MenuContext, UserContext} from '../Context';
-import {Menus} from '../Router';
+class App extends React.Component {
+  state = {
+    anchorEl: null,
+  };
 
-export const Menu = () => (
-  <MenuContext.Consumer>
-    {({isOpen, setOpen, selectedIndex, setSelectedIndex}) => (
-      <UserContext.Consumer>
-        {({name, account}) => (
-          <Drawer modal open={isOpen} onClose={() => setOpen(false)}>
-            <DrawerHeader>
-              <DrawerTitle tag='h2'>{name}</DrawerTitle>
-              <DrawerSubtitle>{account}</DrawerSubtitle>
-            </DrawerHeader>
+  handleMenuOpen = (event) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
-            <DrawerContent>
-              <List singleSelection selectedIndex={selectedIndex}>
-                {Menus.map((menu, index) => (
-                  <ListItem
-                    key={index}
-                    onClick={() => {
-                      setSelectedIndex(index);
-                      setOpen(false);
-                    }}
-                  >
-                    <ListItemGraphic
-                      graphic={<MaterialIcon icon={menu.icon} />}
-                    />
-                    <ListItemText primaryText={menu.text} />
-                  </ListItem>
-                ))}
-              </List>
-            </DrawerContent>
-          </Drawer>
-        )}
-      </UserContext.Consumer>
-    )}
-  </MenuContext.Consumer>
-);
-export default Menu;
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+  };
 
+  render() {
+    const { anchorEl } = this.state;
+    const isMenuOpen = Boolean(anchorEl);
+
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={this.handleMenuOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              Material-UI Menu
+            </Typography>
+            <div style={{ position: 'relative' }}>
+              <div style={{ position: 'absolute', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+                sx={{ pl: 3 }}
+              />
+            </div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={this.handleMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={isMenuOpen}
+              onClose={this.handleMenuClose}
+            >
+              <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+              <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default App;
