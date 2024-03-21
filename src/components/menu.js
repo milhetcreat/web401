c898aa8b00a7d25a3771040f4479a6058c5a1886
Elsa <sasa.milhet@gmail.com>
2024-03-21 18:56:58 +0100

    menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,84 +1,108 @@
-import React from 'react';
-import { AppBar, Typography, Toolbar, IconButton, MenuItem, Menu, InputBase } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Typography, Toolbar, IconButton, MenuItem, Menu, InputBase, Drawer, List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
-class App extends React.Component {
-  state = {
-    anchorEl: null,
+function App() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [leftDrawerOpen, setLeftDrawerOpen] = useState(false);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  handleMenuOpen = (event) => {
-    this.setState({ anchorEl: event.currentTarget });
+  const handleMenuClose = () => {
+    setAnchorEl(null);
   };
 
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
+  const toggleLeftDrawer = (open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setLeftDrawerOpen(open);
   };
 
-  render() {
-    const { anchorEl } = this.state;
-    const isMenuOpen = Boolean(anchorEl);
+  const leftDrawerList = (
+    <div
+      role="presentation"
+      onClick={toggleLeftDrawer(false)}
+      onKeyDown={toggleLeftDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
-    return (
-      <div>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              onClick={this.handleMenuOpen}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              Material-UI Menu
-            </Typography>
-            <div style={{ position: 'relative' }}>
-              <div style={{ position: 'absolute', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
-                sx={{ pl: 3 }}
-              />
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleLeftDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Material-UI Menu
+          </Typography>
+          <div style={{ position: 'relative' }}>
+            <div style={{ position: 'absolute', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+              <SearchIcon />
             </div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={this.handleMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={isMenuOpen}
-              onClose={this.handleMenuClose}
-            >
-              <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
-              <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
-            </Menu>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+            <InputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+              sx={{ pl: 3 }}
+            />
+          </div>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenuOpen}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor={'left'} open={leftDrawerOpen} onClose={toggleLeftDrawer(false)}>
+        {leftDrawerList}
+      </Drawer>
+    </div>
+  );
 }
 
 export default App;
