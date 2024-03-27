af09e8206080921969fa54af0f49e3d98721348a
Elsa <sasa.milhet@gmail.com>
2024-03-27 17:18:17 +0100

    ajout animal

--- src/views/AjoutAnimal.js
+++ src/views/AjoutAnimal.js
@@ -7,14 +7,48 @@ import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom";
 import CardContent from '@mui/material/CardContent';
 import Type from '../components/Type';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { alpha, styled } from '@mui/material/styles';
 
 export default function AjoutAnimal() {
 
 
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '50px' }}>
-      <Type></Type>
-    </div>
-  );
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '50px', gap: '40px' }}>
+            <Type></Type>
+            <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '700px' }, }} noValidate autoComplete="off" style={{ display: 'flex', flexDirection: 'column' }}>
+                <CssTextField label="Nom*" />
+                <CssTextField label="Race*"/>
+                <CssTextField label="Age*" type="number"/>
+                <CssTextField label="Description*" multiline rows={4} />
+                <CssTextField label="Spécificité" multiline rows={3} />
+            </Box>
+        </div>
+    );
 
 }
+
+const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+        color: '#84746A',
+    },
+    '& label': {
+        color: 'black',
+    },
+    '& .MuiInput-underline:after': {
+        borderBottomColor: '#84746A',
+    },
+    '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: '#84746A',
+            borderWidth: 1,
+        },
+        '&:hover fieldset': {
+            borderColor: '#84746A',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: '#84746A',
+        },
+    },
+});
