44a8025e8ffac89e152c194085f39b5d0910cbf5
Elsa <sasa.milhet@gmail.com>
2024-03-30 10:55:52 +0100

    Update AjoutAnimal.js

--- src/views/AjoutAnimal.js
+++ src/views/AjoutAnimal.js
@@ -11,6 +11,7 @@ import MaleIcon from '@mui/icons-material/Male';
 import Button from '@mui/material/Button';
 import Fab from '@mui/material/Fab';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import axios from 'axios';
 
 const AjoutAnimal = () => {
     const [type, setType] = '';
@@ -34,74 +35,58 @@ const AjoutAnimal = () => {
             id_utilisateur: 1,
         };
 
-        componentDidMount() {
-            // Simple POST request with a JSON body using fetch
-            const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ title: 'React POST Request Example' })
-            };
-            fetch('https://milhet.alwaysdata.net/sae401/api/animaux/', requestOptions)
-                .then(response => response.json())
-                .then(data => this.setState({ postId: data.id }));
-        }
-
-        // Envoyer la requête POST à l'API
-        //.post(nouvelAnimal)
-          //  .then(response => {
-            //    setPrenom('');
-              //  setRace('');
-                //setGenre('');
-           //     setAge('');
-             //   setDescription('');
-               // setSpecificite('');
-            //    setType('');
-          //  })
-          //  .catch(error => {
-           //     console.error("Erreur lors de l'ajout de l'animal :", error);
-           // });
+        axios.post(`https://milhet.alwaysdata.net/sae401/api/animaux/`, nouvelAnimal)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+            .catch(error => {
+                console.error("Erreur lors de l'ajout de l'animal :", error);
+            });
     };
 
     return (
-        <div component="form" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '50px', gap: '40px' }} onSubmit={handleSubmit}>
-            <Fab size="medium" color="primary" aria-label="like" style={{ backgroundColor: 'var(--all-stroke)' }}>
-                <CameraAltIcon />
-            </Fab>
-            <Box sx={{ '& > :not(style)': { m: 1, width: '700px' }, }} noValidate autoComplete="off" style={{ display: 'flex', flexDirection: 'column' }}>
-                <Type></Type>
-                <CssTextField label="Prenom*" value={prenom} onChange={(e) => setPrenom(e.target.value)} />
-                <CssTextField label="Race*" value={race} onChange={(e) => setRace(e.target.value)} />
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px', gap: '20px' }}>
-                    <div>
-                        <Card style={{ width: '100px', height: '90px', backgroundColor: genre === 0 ? 'var(--all-stroke)' : 'var(--all-fill)' }} onClick={() => setGenre(0)}>
-                            <CardContent style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                <MaleIcon style={{ fontSize: 60 }} />
-                            </CardContent>
-                        </Card>
-                        <p style={{ color: 'black', textAlign: 'center', margin: '5px 0' }}>Mâle</p>
+        <div>
+            <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '50px', gap: '40px' }}>
+                <Fab size="medium" color="primary" aria-label="like" style={{ backgroundColor: 'var(--all-stroke)' }}>
+                    <CameraAltIcon />
+                </Fab>
+                <Box sx={{ '& > :not(style)': { m: 1, width: '700px' }, }} noValidate autoComplete="off" style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Type></Type>
+                    <CssTextField label="Prenom*" value={prenom} onChange={(e) => setPrenom(e.target.value)} />
+                    <CssTextField label="Race*" value={race} onChange={(e) => setRace(e.target.value)} />
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px', gap: '20px' }}>
+                        <div>
+                            <Card style={{ width: '100px', height: '90px', backgroundColor: genre === 0 ? 'var(--all-stroke)' : 'var(--all-fill)' }} onClick={() => setGenre(0)}>
+                                <CardContent style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    <MaleIcon style={{ fontSize: 60 }} />
+                                </CardContent>
+                            </Card>
+                            <p style={{ color: 'black', textAlign: 'center', margin: '5px 0' }}>Mâle</p>
+                        </div>
+                        <div>
+                            <Card style={{ width: '100px', height: '90px', backgroundColor: genre === 1 ? 'var(--all-stroke)' : 'var(--all-fill)' }} onClick={() => setGenre(1)}>
+                                <CardContent style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    <FemaleIcon style={{ fontSize: 60 }} />
+                                </CardContent>
+                            </Card>
+                            <p style={{ color: 'black', textAlign: 'center', margin: '5px 0' }}>Femelle</p>
+                        </div>
                     </div>
-                    <div>
-                        <Card style={{ width: '100px', height: '90px', backgroundColor: genre === 1 ? 'var(--all-stroke)' : 'var(--all-fill)' }} onClick={() => setGenre(1)}>
-                            <CardContent style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                <FemaleIcon style={{ fontSize: 60 }} />
-                            </CardContent>
-                        </Card>
-                        <p style={{ color: 'black', textAlign: 'center', margin: '5px 0' }}>Femelle</p>
+                    <CssTextField label="Age*" type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+                    <CssTextField label="Localisation*" value={localisation} onChange={(e) => setLocalisation(e.target.value)} />
+                    <CssTextField label="Description*" multiline rows={4} value={description} onChange={(e) => setDescription(e.target.value)} />
+                    <CssTextField label="Spécificité" multiline rows={3} value={specificite} onChange={(e) => setSpecificite(e.target.value)} />
+                    <div style={{ display: 'flex', gap: '20px', marginTop: '70px' }}>
+                        <Button type="submit" variant="contained" size="medium" style={{ backgroundColor: 'var(--all-stroke)' }}>
+                            Valider
+                        </Button>
+                        <Button variant="outlined" size="medium" style={{ borderColor: 'var(--all-stroke)', color: 'var(--all-stroke)' }}>
+                            Annuler
+                        </Button>
                     </div>
-                </div>
-                <CssTextField label="Age*" type="number" value={age} onChange={(e) => setAge(e.target.value)} />
-                <CssTextField label="Localisation*" value={localisation} onChange={(e) => setLocalisation(e.target.value)} />
-                <CssTextField label="Description*" multiline rows={4} value={description} onChange={(e) => setDescription(e.target.value)} />
-                <CssTextField label="Spécificité" multiline rows={3} value={specificite} onChange={(e) => setSpecificite(e.target.value)} />
-                <div style={{ display: 'flex', gap: '20px', marginTop: '70px' }}>
-                    <Button type="submit" variant="contained" size="medium" style={{ backgroundColor: 'var(--all-stroke)' }}>
-                        Valider
-                    </Button>
-                    <Button variant="outlined" size="medium" style={{ borderColor: 'var(--all-stroke)', color: 'var(--all-stroke)' }}>
-                        Annuler
-                    </Button>
-                </div>
-            </Box>
+                </Box>
+            </form>
         </div>
     );
 }
