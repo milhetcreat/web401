c85de5b2de9bd126adb90b2498039ac60da9a731
Elsa <sasa.milhet@gmail.com>
2024-04-10 10:58:30 +0200

    mon espace

---
+++ src/components/animauxUser.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+//import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { AlignHorizontalCenter } from "@mui/icons-material";
+import Fab from '@mui/material/Fab';
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import EditIcon from '@mui/icons-material/Edit';
+import '../colors.css';
+
+export default function ListAnimauxUser() {
+    //const { idUser } = useParams();
+    const idUser = 1;
+    const [user, setUser] = useState(null);
+
+    //const idUser = localStorage.getItem('user_id');
+    const token = localStorage.getItem('accessToken');
+    const url = `https://milhet.alwaysdata.net/sae401/api/animaux/${idUser}/users`;
+    const [listeAnimaux, setAnimaux] = useState([]);
+
+    useEffect(() => {
+        const fetchAnimaux = () => {
+            const fetchOptions = { method: "GET" };
+            fetch(url, fetchOptions)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((dataJSON) => {
+                    setAnimaux(dataJSON);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        };
+
+        fetchAnimaux();
+
+    }, [url]);
+
+
+
+    return (
+        <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', justifyContent: 'center', marginTop: '10px' }}>
+            {listeAnimaux.map((animal) => (
+                <Card variant="outlined" sx={{ maxWidth: 600 }}>
+                    <Box sx={{ p: 2 }}>
+                        <Stack direction="row" justifyContent="space-around" gap="20px">
+                            <img
+                                width="180px"
+                                src={"https://milhet.alwaysdata.net/sae401/images/" + animal.PHOTO}
+                                alt={animal.PRENOM}
+                            />
+                            <Stack direction="column" marginRight="50px">
+                                <Typography gutterBottom variant="h6" component="div" style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                                    {`${animal.PRENOM} `}
+                                    {animal.GENRE === 0 ? <MaleIcon /> : <FemaleIcon />}
+                                </Typography>
+                                <div style={{ display: 'flex', gap: '20px', marginTop: '15px' }}>
+                                    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '5px'}}>
+                                        <Fab size="tall" style={{ backgroundColor: 'var(--all-fill)', color: 'white' }}>
+                                            <FavoriteBorderIcon />
+                                        </Fab>
+                                        <Typography>toto</Typography>
+                                    </div>
+                                    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '5px'}}>
+                                        <Fab size="tall" style={{ backgroundColor: 'var(--all-fill)', color: 'white' }}>
+                                            <EditIcon />
+                                        </Fab>
+                                        <Typography>Edit</Typography>
+                                    </div>
+                                </div>
+                            </Stack>
+                        </Stack>
+                    </Box>
+                </Card>
+            ))}
+        </div>
+
+
+    );
+}
--- src/views/MonEspace.js
+++ src/views/MonEspace.js
@@ -21,90 +21,24 @@ import PetsIcon from '@mui/icons-material/Pets';
 import DateRangeIcon from '@mui/icons-material/DateRange';
 import ReportIcon from '@mui/icons-material/Report';
 import '../colors.css';
+import InfoUser from "../components/infoUser";
+import ListAnimauxUser from "../components/animauxUser";
+import AddIcon from '@mui/icons-material/Add';
 
 export default function MonEspace() {
-    //const { idUser } = useParams();
-    //const idUser = 1;
-    const [user, setUser] = useState(null);
-
-    const idUser = localStorage.getItem('user_id');
-    const token = localStorage.getItem('accessToken');
-
-    useEffect(() => {
-        const fetchInfoUser = () => {
-            fetch(
-                `https://milhet.alwaysdata.net/sae401/api/utilisateurs/${idUser}`   
-            )
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error("Network response was not ok");
-                    }
-                    return response.json();
-                })
-                .then((data) => {
-                    setUser(data);
-                })
-                .catch((error) => {
-                    console.error("Error fetching user infos:", error);
-                });
-        };
-
-        fetchInfoUser();
-    }, [idUser]);
 
     return (
-        <div>
-            {user && (
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <Breadcrumbs aria-label="breadcrumb" style={{ marginLeft: '250px', marginTop: '20px', marginBottom: '20px' }}>
-                        <Link underline="hover" color="inherit" href="/">
-                            Home
-                        </Link>
-                        <Typography color="text.primary">Mon Espace</Typography>
-                    </Breadcrumbs>
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginLeft: '250px', marginRight: '250px' }}>
-                        <h3 className="prenom" style={{ fontSize: '65px', marginBottom: '5px', marginTop: '0px', color: 'var(--dog-stroke)' }}>{user.PRENOM}</h3>
-                        <div>
-                            <Fab size="small" aria-label="like" style={{ marginRight: '5px', backgroundColor: 'var(--dog-stroke)', color: 'white' }}>
-                                <FavoriteIcon />
-                            </Fab>
-                            <Fab size="small" aria-label="like" style={{ backgroundColor: 'var(--dog-stroke)', color: 'white' }}>
-                                <ReportIcon />
-                            </Fab>
-                        </div>
-                    </div>
-                    <div style={{ backgroundColor: 'var(--bg-user)', display: 'flex', gap: '150px', padding: '50px', justifyContent: 'center', alignItems: 'center' }}>
-                        <div>
-                            <img
-                                width="400px"
-                                src={"https://milhet.alwaysdata.net/sae401/images/" + user.pdp}
-                                alt={user.prenom}
-                            />
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'column' }}>
-                            <p style={{ display: 'flex', alignItems: 'center', gap: '5px' }}><PetsIcon></PetsIcon> {user.RACE}</p>
-                            <p style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>{user.genre === 0 ? (<> <MaleIcon /> Mâle </>) : (<>  <FemaleIcon /> Femelle </>)}</p>
-                            <p style={{ display: 'flex', alignItems: 'center', gap: '5px' }}> <DateRangeIcon></DateRangeIcon> {user.AGE} {user.AGE === 1 ? 'an' : 'ans'}</p>
-                            <p style={{ display: 'flex', alignItems: 'center', gap: '5px', fontWeight: 'bold' }}><LocationOnIcon></LocationOnIcon> {user.LOCALISATION}</p>
-                        </div>
-                    </div>
-                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'start', marginLeft: '250px', marginRight: '250px', marginTop: '50px' }}>
-                        <div style={{ marginLeft: '250px', marginRight: '250px' }}>
-                            <p>{user.DESCRIPTION}</p>
-                            <p style={{ fontWeight: 'bold' }}> Spécificités</p>
-                            <p>{user.SPECIFICITE === null ? 'Aucunes' : user.SPECIFICITE}</p>
-                        </div>
-                        <ListItem>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <ImageIcon />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary="Photos" secondary={user.PRENOM}/>
-                        </ListItem>
-                    </div>
-                </div>
-            )}
+        <div style={{ marginRight: '100px', marginLeft: '100px' }}>
+            <p>Info User</p>
+            <p style={{fontSize: '22px', fontWeight: '500'}}>Mes Animaux</p>
+            <ListAnimauxUser></ListAnimauxUser>
+            <div style={{textAlign: 'center', marginTop: '50px'}}>
+            <Link className='Link' to={"/ajoutanimal"}>
+                <Fab size="medium" style={{ backgroundColor: 'var(--all-stroke)', color: 'white' }}>
+                    <AddIcon />
+                </Fab>
+            </Link>
+            </div>
         </div>
     );
 }
